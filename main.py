89e41321f922ab6d01f034b943c76fd3d30ef395
zovzovzov370 <huba6577@gmail.com>
2025-11-02 23:35:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -783,88 +783,127 @@ print("результат: ", result)
 #     print(f"результат: {result}")
 #     print(f"фиксированный элемент: {arr[k]} остался на позиции {k}")
 
-points_to_letters = {
-    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'С', 'Т'],
-    2: ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'],
-    3: ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'],
-    4: ['F', 'H', 'V', 'W', 'Y', 'Р', 'Й', 'Р' 'Ы'],
-    5: ['K', 'Ж', 'З', 'Ч', 'Ц'],
-    8: ['J', 'X', 'Ш', 'Э', 'Ю', 'Х' 'Х'],
-    10: ['Q', 'Z', 'Ф', 'Щ', 'Ъ']
-}
-gamers = {} #словарь игрока
-count_user = int(input("сколько человек будет играть?/:"))
-for i in range(count_user):
-    name = input("введите имя игрока:")
-    gamers[name] = 0
-print(f"писок игроков: \n{gamers}")
-# result = 0
-# 10 раундов
-for raund in range(10):
-    for gamer in gamers.keys():
-        print("*"*11)
-        print(f"ходит игрок {gamer}")
-        answer = input("введите слово:")
-        for i in answer:
-            for key, value in dictionnary.items():
-                if i in value:
-                    gamers[gamer] += key
-print("игра окончена! \n таблица игроков:")
-for key, value in dictionnary.items():
-    print(f"{key} -> {value} баллов")
-result_user = ' '
-result_user = ' '
-for key, value in gamers.items():
-    if result_value < value:
-        result_value = value
-        result_user = key
-print(f"победитель: {result_user}")
-from sys import intern
+#points_to_letters = {
+#     1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'С', 'Т'],
+#     2: ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'],
+#     3: ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'],
+#     4: ['F', 'H', 'V', 'W', 'Y', 'Р', 'Й', 'Р' 'Ы'],
+#     5: ['K', 'Ж', 'З', 'Ч', 'Ц'],
+#     8: ['J', 'X', 'Ш', 'Э', 'Ю', 'Х' 'Х'],
+#     10: ['Q', 'Z', 'Ф', 'Щ', 'Ъ']
+# }
+# gamers = {} #словарь игрока
+# count_user = int(input("сколько человек будет играть?/:"))
+# for i in range(count_user):
+#     name = input("введите имя игрока:")
+#     gamers[name] = 0
+# print(f"писок игроков: \n{gamers}")
+# # result = 0
+# # 10 раундов
+# for raund in range(10):
+#     for gamer in gamers.keys():
+#         print("*"*11)
+#         print(f"ходит игрок {gamer}")
+#         answer = input("введите слово:")
+#         for i in answer:
+#             for key, value in dictionnary.items():
+#                 if i in value:
+#                     gamers[gamer] += key
+# print("игра окончена! \n таблица игроков:")
+# for key, value in dictionnary.items():
+#     print(f"{key} -> {value} баллов")
+# result_user = ' '
+# result_user = ' '
+# for key, value in gamers.items():
+#     if result_value < value:
+#         result_value = value
+#         result_user = key
+# print(f"победитель: {result_user}")
+# from sys import intern
+#
+# backpack = {'зажигалка':20, 'компас':100, 'фрукты':500, 'рубашка':300,
+#             'термос':1000, 'аптечка':200, 'куртка':600, 'бинокль':400,
+#             'удочка':1300, 'салфетки':40, 'бутерброды':800, 'палатка':5500,
+#             'спальный мешок':2500, 'изолента':250, 'котел':3000
+# }
+# max_mass = int(input("введитемаксимальный вес для похода:"))
+# inter_mass = 0
+# while iner_mass < max_mass:
+#     print(backpack)
+#     answer = input("что вы хотите взять с собой:")
+#     for key, value in backpack.items():
+#         if key == answer:
+#             iner_mass += backpack[key]
+# print(f"рюкзак заполнен, текущая масса:{iner_mass}")
+#
+# note_book = {
+#     "Маша": {
+#         'tel': '+7922123561',
+#         'vk': 'vk.com/masha321',
+#         'youtube': 'youtube.com/masha321',
+#         'telegram': 't.me/masha321'
+#     },
+#     "Паша": {
+#         'tel': '+7922123562',
+#         'vk': 'vk.com/pasha123',
+#         'youtube': 'youtube.com/pasha123',
+#         'telegram': 't.me/pasha123'
+#     },
+#     "Саша": {
+#         'tel': '+7922123563',
+#         'vk': 'vk.com/sasha456',
+#         'youtube': 'youtube.com/sasha456',
+#         'telegram': 't.me/sasha456'
+#     }
+# }
+# print("доступные контакты:", ", ".join(note_book.keys()))
+#
+# # Запрашиваем имя для поиска
+# user_search = input("введите имя из списка контактов: ").capitalize()
+#
+# # Проверяем, есть ли контакт в книге и выводим информацию
+# if user_search in note_book:
+#     print(f"\nинформация о контакте {user_search}:")
+#     for key, value in note_book[user_search].items():
+#         print(f"{key}: {value}")
+# else:
+#     print(f"контакт '{user_search}' не найден в телефонной книге.")
 
-backpack = {'зажигалка':20, 'компас':100, 'фрукты':500, 'рубашка':300,
-            'термос':1000, 'аптечка':200, 'куртка':600, 'бинокль':400,
-            'удочка':1300, 'салфетки':40, 'бутерброды':800, 'палатка':5500,
-            'спальный мешок':2500, 'изолента':250, 'котел':3000
-}
-max_mass = int(input("введитемаксимальный вес для похода:"))
-inter_mass = 0
-while iner_mass < max_mass:
-    print(backpack)
-    answer = input("что вы хотите взять с собой:")
-    for key, value in backpack.items():
-        if key == answer:
-            iner_mass += backpack[key]
-print(f"рюкзак заполнен, текущая масса:{iner_mass}")
+print('задание 1')
+print('"Don`t compare yourself with anyone in this world...if you do so, you are insulting\n'
+      'yourself."\n'
+      '\n'
+      'Bill Gates')
 
-note_book = {
-    "Маша": {
-        'tel': '+7922123561',
-        'vk': 'vk.com/masha321',
-        'youtube': 'youtube.com/masha321',
-        'telegram': 't.me/masha321'
-    },
-    "Паша": {
-        'tel': '+7922123562',
-        'vk': 'vk.com/pasha123',
-        'youtube': 'youtube.com/pasha123',
-        'telegram': 't.me/pasha123'
-    },
-    "Саша": {
-        'tel': '+7922123563',
-        'vk': 'vk.com/sasha456',
-        'youtube': 'youtube.com/sasha456',
-        'telegram': 't.me/sasha456'
-    }
-}
-print("доступные контакты:", ", ".join(note_book.keys()))
+print('задание 2')
 
-# Запрашиваем имя для поиска
-user_search = input("введите имя из списка контактов: ").capitalize()
+def get_even_numbers(num1, num2):
+    start = min(num1, num2)
+    end = max(num1, num2)
+    even_numbers = []
+    for number in range(start, end + 1):
+        if number % 2 == 0:
+            even_numbers.append(number)
+    return even_numbers
+result = get_even_numbers(3, 12)
+print(f"четные числа: {result}")
 
-# Проверяем, есть ли контакт в книге и выводим информацию
-if user_search in note_book:
-    print(f"\nинформация о контакте {user_search}:")
-    for key, value in note_book[user_search].items():
-        print(f"{key}: {value}")
-else:
-    print(f"контакт '{user_search}' не найден в телефонной книге.")
+print('задание 3')
+
+def xz(side_length, symbol, is_filled):
+    for row in range(side_length):
+        line = ""
+        for col in range(side_length):
+            if is_filled:
+                line += symbol + " "
+            else:
+                if row == 0 or row == side_length - 1 or col == 0 or col == side_length - 1:
+                    line += symbol + " "
+                else:
+                    line += "  "
+        print(line)
+print("заполненный квадрат 6x6:")
+xz(6, '#', True)
+
+print("\nпустой квадрат 6x6:")
+xz(6, '#', False)
